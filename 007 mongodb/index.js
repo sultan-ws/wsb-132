d8a2031861ff1310d870ef4257852fdfe80763ed
sultanBytes <sultankhand11@gmail.com>
2025-01-01 11:23:28 +0530

    mongodb crud

--- 007 mongodb/index.js
+++ 007 mongodb/index.js
@@ -1,10 +1,11 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://sultankhan:sj1XtNfoMKK1If1T@sultan.luvya.mongodb.net/?retryWrites=true&w=majority&appName=sultan';
 
 const client = new MongoClient(url);
 
-const connection = async ()=>{
+const connection = async () => {
     await client.connect();
     const db = client.db('wsb_132_tmp');
 
@@ -19,17 +20,60 @@ const connection = async ()=>{
     }
 };
 
-const createUser = async ()=>{
-    const {User} = await connection();
-    
+const createUser = async () => {
+    const { User } = await connection();
+
     const data = {
-        name: 'John Doe',
+        name: 'janny Doe',
         age: 30,
-        conatct:'john@mail.com'
+        conatct: 'john@mail.com'
     };
 
     const response = await User.insertOne(data);
     console.log(response);
 };
 
-createUser();
+// createUser();
+
+const readUsers = async () => {
+    const { User } = await connection();
+    const response = await User.find().toArray();
+    console.log(response);
+};
+
+readUsers();
+
+const updateUser = async () => {
+    const { User } = await connection();
+    const response = await User.updateOne(
+        { _id: new ObjectId('6774d460b470cd7396ae2524') },
+        {
+            $unset: {
+                age: '32',
+            }
+        }
+    );
+
+
+    // const response = await User.updateOne(
+    //     { _id: new ObjectId('6774d460b470cd7396ae2524')},
+    //     {
+    //         $set:{
+    //             age:32,
+    //             conatct:'johnupdated@mail.com'
+    //         }
+    //     }
+    // );
+
+    console.log(response);
+};
+
+// updateUser();
+
+const deleteUser = async ()=>{
+    const { User } = await connection();
+    const response = await User.deleteOne({_id: new ObjectId('6774d54c147c63200bbd38a5')});
+    console.log(response);
+};
+
+// deleteUser();
